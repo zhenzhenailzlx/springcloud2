e08418db6680cbc6bac653df26808ce2a9afc9d9
zhen.yin <zhen.yin@git.100credit.cn>
2020-11-30 15:04:40 +0800

    新建项目

---
+++ springcloud-gateway/src/main/java/com/zhenzhen/demo/springcloud2/gateway/GateWayApplication.java
@@ -0,0 +1,26 @@
+package com.zhenzhen.demo.springcloud2.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+@EnableCircuitBreaker
+public class GateWayApplication {
+
+	@RequestMapping("/ping")
+	public String getOrder() {
+		return "gateway tong"+System.currentTimeMillis();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(GateWayApplication.class, args);
+	}
+
+}
